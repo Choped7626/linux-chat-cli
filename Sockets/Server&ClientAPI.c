24dfe58394db365433cc47e9afa9d7b5604b6108
Choped7626 <130707856+Choped7626@users.noreply.github.com>
2024-07-12 18:23:31 +0200

    Actualizacion
    ahora todos ven los mensajes y tal , principal problema q creo q el main thread del server solo se cierra si se obtiene el numero maximo de clientes y lo hace crasheando

--- Sockets/Server&ClientAPI.c
+++ Sockets/Server&ClientAPI.c
@@ -6,8 +6,9 @@ void gilipollas(const char *numError){
 }
 
 struct args{
-    int id_Cliente;
+    char* nombreCliente;
     int novoCalcetinFD;
+    int* DFsClientes;
 };
 
 struct info_Fio{
@@ -15,40 +16,68 @@ struct info_Fio{
     struct args *args;
 };
 
+int numeroDeClientes = 0;
+
 void *lanzarFioServer(void *ptr){
     struct args *args = ptr;
-    long cnt;
     char buffer[255];
     char nombre[32];
-    int i = 0;
+    char send[287];
+    long datosLeidosEscritos;
+    int PrimeraIteracion = 0;
+    int comparacion = -1;
 
     while(1){
         bzero(buffer, 255);
-        cnt = read(args->novoCalcetinFD, buffer, 255);
-        if(cnt < 0)
+        datosLeidosEscritos = read(args->novoCalcetinFD, buffer, 255);
+        if(datosLeidosEscritos < 0)
             gilipollas("Se te jodio el mensaje tipo");
-        if(i == 0){
-            for (int j = 0; j < 32; ++j) {
-                if (buffer[j] == '\n')
-                    break;
-                nombre[j] = buffer[j];
+        if(PrimeraIteracion == 0){
+            char* tok = strtok(buffer, "\n");
+            strcpy(nombre, tok);
+            PrimeraIteracion++;
+            sprintf(send , "%s connected to the chat\n", nombre);
+            printf("%s", send);
+        }else {
+            sprintf(send, "%s: %s", nombre, buffer);
+            printf("%s", send);
+            comparacion = strncmp("Exit", buffer, 4);
+        }
+            int iterator = 0;
+            while (iterator < CLIENTES){
+                if (args->DFsClientes[iterator] != -1){
+                    if(comparacion == 0){
+                        sprintf(send, "%s left the chat\n" , nombre);
+                        datosLeidosEscritos = write(args->DFsClientes[iterator] , send, 287);
+                        if(args->DFsClientes[iterator] == args->novoCalcetinFD){
+                            close(args->DFsClientes[iterator]);
+                            args->DFsClientes[iterator] = -1;
+                        }
+                    }else{
+                        if(args->DFsClientes[iterator] != args->novoCalcetinFD)
+                            datosLeidosEscritos = write(args->DFsClientes[iterator] , send, 287);
+                    }
+                    if(datosLeidosEscritos < 0)
+                        gilipollas("Se te jodio el mensaje tipo");
+                }
+                iterator++;
             }
-            i++;
-            printf("%s connected to the chat\n", nombre);
-        }else{
-            i = strncmp("Exit" , buffer , 4);///cambiar este texto e numero de caracteres para cambiar mensaje de salir
-            if (i == 0){
+            if (comparacion == 0){
                 printf("%s left the chat\n", nombre);
+                numeroDeClientes--;
                 break;
             }
-            printf("%s: %s", nombre, buffer);
-        }
+
     }
     close(args->novoCalcetinFD);
     return NULL;
 }
 
 void lanzarServer(int nomeroPorto) {
+
+    int DFsClientes[CLIENTES];
+    for (int i = 0; i < CLIENTES; ++i)
+        DFsClientes[i] = -1;
 
     if (nomeroPorto <= 1024) {
         fprintf(stderr, "Formato: ServerThreadTCP <Numero de puerto>\n");
@@ -66,10 +95,6 @@ void lanzarServer(int nomeroPorto) {
     struct sockaddr_in server_Address, client_Address;
     int soquetFD, novoSoquetFD;
     socklen_t clienteLenght;
-    struct timeval timeout;
-
-    timeout.tv_sec = TIMEOUT;
-    timeout.tv_usec = 0;
 
     soquetFD = socket(AF_INET, SOCK_STREAM, 0);
     if (soquetFD < 0)
@@ -87,31 +112,33 @@ void lanzarServer(int nomeroPorto) {
     listen(soquetFD, CLIENTES);
     clienteLenght = sizeof(client_Address);
 
-    int n = 0;
     while (1) {
 
         novoSoquetFD = accept(soquetFD, (struct sockaddr *) &client_Address, &clienteLenght);
 
+        DFsClientes[numeroDeClientes] = novoSoquetFD;
+
         if (novoSoquetFD < 0)
             gilipollas("Rechazao subnormal");
 
-        fios[n].args = malloc(sizeof(struct args));
-        fios[n].args->id_Cliente = n;
-        fios[n].args->novoCalcetinFD = novoSoquetFD;
+        fios[numeroDeClientes].args = malloc(sizeof(struct args));
+        //fios[numeroDeClientes].args->id_Cliente = numeroDeClientes;
+        fios[numeroDeClientes].args->DFsClientes = DFsClientes;
+        fios[numeroDeClientes].args->novoCalcetinFD = novoSoquetFD;
 
-        if (0 != pthread_create(&fios[n].id, NULL, lanzarFioServer, fios[n].args)) {
-            printf("Could not create thread #%d", n);
+        if (0 != pthread_create(&fios[numeroDeClientes].id, NULL, lanzarFioServer, fios[numeroDeClientes].args)) {
+            printf("Could not create thread #%d", numeroDeClientes);
             exit(1);
         }
-        n++;
+        numeroDeClientes++;
 
-        if (n == CLIENTES) {
+        if (numeroDeClientes == CLIENTES) {
             printf("Limite de clientes alcanzado\n");
             break;
         }
     }
 
-    for (int i = 0; i < n; ++i){
+    for (int i = 0; i < numeroDeClientes; ++i){
         pthread_join(fios[i].id, NULL);
         free(fios[i].args);
     }
@@ -121,9 +148,35 @@ void lanzarServer(int nomeroPorto) {
     close(soquetFD);
 }
 
+void* recibirMensajes(void* ptr){////
+    struct args *args = ptr;
+    char buffer[287];
+
+    long cnt;
+    int i;
+
+    char salir[287];
+    sprintf(salir, "%s left the chat\n" , args->nombreCliente);
+
+    while(1){
+        bzero(buffer, 287);
+        cnt = read(args->novoCalcetinFD, buffer, 287);
+
+        if(cnt < 0)
+            gilipollas("Se te jodio el mensaje tipo");
+
+        printf("%s", buffer);
+        i = strcmp(salir , buffer);
+        if (i == 0)
+            break;
+    }
+    close(args->novoCalcetinFD);
+    return NULL;
+}
+
 void lanzarCliente(char* hostName, int nomeroPorto){
     struct sockaddr_in server_Address;
-    int soquetFD, cnt;
+    int soquetFD; long cnt;
     struct hostent *server;
     char buffer[255];
     char nombre[32];
@@ -158,22 +211,39 @@ void lanzarCliente(char* hostName, int nomeroPorto){
     if(cnt < 0)
         gilipollas("Fallo en escritura");
 
+    struct info_Fio *fiosCliente;
+    fiosCliente = malloc(sizeof(struct info_Fio));
+
+    fiosCliente->args = malloc(sizeof(struct args));
+    fiosCliente->args->nombreCliente = nombre;
+    fiosCliente->args->novoCalcetinFD = soquetFD;
+
+    if (0 != pthread_create(&fiosCliente->id, NULL, recibirMensajes, fiosCliente->args)) {
+        printf("Could not create thread");
+        exit(1);
+    }
+
     while(1){
         bzero(buffer, 255);
         fgets(buffer, 255, stdin);
         int i = strncmp("Exit", buffer, 4);
 
-        cnt = write(soquetFD, buffer, strlen(buffer));
+        if(buffer[0] != '\n')
+            cnt = write(soquetFD, buffer, strlen(buffer));
         if(cnt < 0)
             gilipollas("Fallo en escritura");
 
         if(i == 0){
-            cnt = write(soquetFD, buffer, strlen(buffer));
+            //?¿?¿?¿?cnt = write(soquetFD, buffer, strlen(buffer));
             if(cnt < 0)
                 gilipollas("Fallo en escritura");
             break;
         }
     }
+
+    pthread_join(fiosCliente->id, NULL);
+    free(fiosCliente->args);
+    free(fiosCliente);
     close(soquetFD);
 }
 
